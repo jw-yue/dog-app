bef82254efcc5a37567b5b1a4c673857bd3a81aa
Julie Yue <julie.yue.2011@gmail.com>
2022-04-04 11:10:36 -0500

    changed routing

--- src/App.js
+++ src/App.js
@@ -2,18 +2,15 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 //import pages
 import Home from './pages/Home'
-import SingleDog from './pages/SingleDog'
+// import SingleDog from './pages/SingleDog'
 import Error from './pages/Error'
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/">
+        <Route exact path="/dog">
           <Home />
-        </Route>
-        <Route path="/dog/:id">
-          <SingleDog />
         </Route>
         <Route path="*">
           <Error />
