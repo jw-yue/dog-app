f3aafdcc4d9c7eabd0901b44077b7ccb85d3e55b
Julie Yue <julie.yue.2011@gmail.com>
2022-04-04 11:17:08 -0500

    fixing routing for gh-pages

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/dog">
+        <Route exact path="/">
           <Home />
         </Route>
         <Route path="*">
