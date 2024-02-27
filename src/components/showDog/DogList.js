86ad1426ce66e76fa984e4fb8f1d94fc2c6483de
Julie Yue <julie.yue.2011@gmail.com>
2024-02-26 18:37:47 -0600

    -rebuild app

--- src/components/showDog/DogList.js
+++ src/components/showDog/DogList.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import DogCard from './Dog'
-import Loading from '../loading/Loading'
-import { useGlobalContext } from '../../context/context'
-import AddDog from '../dogManagment/AddDog'
+import React from "react";
+import DogCard from "./Dog";
+import Loading from "../loading/Loading";
+import { useGlobalContext } from "../../context/context";
+import AddDog from "../dogManagment/AddDog";
 
 const DogList = () => {
-  const { dogs, loading } = useGlobalContext()
+  const { dogs, loading } = useGlobalContext();
 
   if (loading) {
-    return <Loading />
+    return <Loading />;
   }
   if (dogs.length < 1) {
     return (
@@ -18,7 +18,7 @@ const DogList = () => {
         </div>
         <h2 className="section-title">no dogs match your search criteria</h2>
       </section>
-    )
+    );
   }
 
   return (
@@ -26,14 +26,14 @@ const DogList = () => {
       <div className="dog-section">
         <AddDog />
       </div>
-      <h2 className="section-title">Dogs of Voyant</h2>
+      <h2 className="section-title">Dogs Directory</h2>
       <div className="dogs-center">
         {dogs.map((item) => {
-          return <DogCard key={item.id} {...item} />
+          return <DogCard key={item.id} {...item} />;
         })}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default DogList
+export default DogList;
